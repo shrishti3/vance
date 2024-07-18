e3f512b1ffad7babfe0f756d80129ac165f7f440
SHRISHTI <sg2195@srmist.edu.in>
2024-07-18 23:27:09 +0530

    vercel

--- server/api.py
+++ server/api.py
@@ -4,6 +4,10 @@ from database import get_db
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
+@api_bp.route('/', methods=['GET'])
+def exp():
+    return'Hello world'
+
 @api_bp.route('/forex-data', methods=['GET','POST'])
 def forex_data():
     from_currency = request.args.get('from')
